4e29cfc0d2cced1e920aa2e7ab0c52b632f11167
James Mulholland <james@jmulholland.com>
2018-08-26 11:26:22 +0100

    add check empty and check valid functions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys
 
 # board states take the form of 9 char strings
@@ -11,9 +13,70 @@ class BoardState:
     CROSSES_WIN='CROSSES_WIN'
     DRAW='DRAW'
 
+class BoardElements:
+    X='X'
+    O='X'
+    _='_'
 
 def getStateOfBoard(board):
-    return BoardState.DRAW
+    """Returns the current state of a given board string"""
+
+    def checkIsEmpty(board):
+        """Checks if a given board is empty"""
+        if board == '_________':
+            return True
+        else:
+            return False
+
+    def checkIsValid(board):
+        """Checks if players could have arrived at the current board"""
+        
+        def elementDifference(board):
+            """Returns the difference between #crosses and #noughts"""
+
+            def countElement(element, board):
+                """Count the number of noughts, crosses or empties"""
+                count = 0
+                for char in board:
+                    if char == element:
+                        count = count + 1
+                return count
+
+            numCrosses = countElement(BoardElements.X, board)
+            numNoughts = countElement(BoardElements.O, board)
+            return numCrosses - numNoughts
+
+        difference = elementDifference(board)
+        if -1 <= difference <= 1:
+            return True
+        else:
+            return False
+
+    def checkWin(element, board):
+        return
+
+    # elif checkNoughtsWin(board):
+        # return BoardState.NOUGHTS_WIN
+
+    # elif checkCrossesWin(board):
+        # return BoardState.CROSSES_WIN
+
+    # elif checkDraw(board):
+        # return BoardState.DRAW
+
+    # catch any other possibilities that I may have forgotten
+    # return error
+
+    if checkIsEmpty(board):
+        return BoardState.EMPTY
+    elif not checkIsValid(board):
+        return BoardState.INVALID
+    # elif checkWin(BoardElements.X, board):
+        # return BoardState.CROSSES_WIN
+    # elif checkWin(BoardElements.O, board):
+        # return BoardState.NOUGHTS_WIN
+    else:
+        raise ValueError("Board does not match any of the states")
 
 # leave unchanged
 for arg in sys.argv[1:]:
--- todo.md
+++ todo.md
@@ -1,6 +1,9 @@
 # todo
-- [ ] Define possible board states
+- [x] Define possible board states
 - [ ] Plan out algorithim in notebook
 - [ ] Implement in python
+- [ ] Test with different example boards
 - [ ] Upload to github (private)
 
+## validity
+* A board is valid if the difference between the two values is not greater than 1
